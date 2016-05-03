368f8483ba255a13976b08e7e450f313a5bd209b
Vasco Silva <vasco.silva@tekzenit.com>
2016-05-03 20:11:35 +0100

    Add add and remove class methods, add comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+/*
+   __   __  _______  ___   ___      ___      _______
+  |  | |  ||       ||   | |   |    |   |    |   _   |
+  |  | |  ||_     _||   | |   |    |   |    |  |_|  |
+  |  |_|  |  |   |  |   | |   |    |   |    |       |
+  |       |  |   |  |   | |   |___ |   |___ |       |
+  |       |  |   |  |   | |       ||       ||   _   |
+  |_______|  |___|  |___| |_______||_______||__| |__|
+
+  A lightweight, vanilla js, dom manipulation library
+  Authors: Pedro Brás (https://github.com/pedromrb) & Vasco Silva (https://github.com/vascocsilva)
+
+  Version: 1.0.0
+*/
+
 class Utilla {
   constructor(selector) {
     this.elements = document.querySelectorAll(selector);
@@ -22,6 +37,37 @@ class Utilla {
   show(displayType = '') {
     this.each(function() {
       this.style.display = displayType;
+    });
+
+    return this;
+  }
+
+  // Add a class to current selected elements
+  // @param className (string)
+  addClass(className) {
+    this.each(function() {
+      if (this.classLlist)
+        this.classList.add(className);
+      else {
+        this.className += ' ' + className;
+      }
+    });
+
+    return this;
+  }
+
+  // Remove a class in current selected elements
+  // @param className (string)
+  removeClass(className) {
+    this.each(function() {
+      if (this.classList)
+        this.classList.remove(className);
+      else
+        this.className = this.className.replace(
+          new RegExp('(^|\\b)'
+          + className.split(' ').join('|')
+          + '(\\b|$)', 'gi'), ' '
+        );
     });
 
     return this;
